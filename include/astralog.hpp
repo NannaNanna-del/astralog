eb6c2f6d69cbade65bca0c55f3294436add8109f
swampholyten <ji.junkai@outlook.com>
2026-05-30 13:15:47 +0200

    add system config (todo)

--- include/astralog.hpp
+++ include/astralog.hpp
@@ -9,9 +9,20 @@
 
 namespace astralog {
 
+// Factory for creating pipelines
 class PipelineFactory {
 public:
   static std::unique_ptr<pipeline::SequentialPipeline> create_sequential();
+
+  // TODO
+  // static std::unique_ptr<pipeline::SequentialPipeline> create_parallel();
+};
+
+// System initializer - loads configuration and sets up environment
+class SystemInitializer {
+public:
+  static bool load_sensor_config(const std::string &yaml_path);
+  static bool load_rules_config(const std::string &json_path);
 };
 
 } // namespace astralog
---
+++ src/astralog.cpp
@@ -0,0 +1,15 @@
+#include "astralog.hpp"
+
+namespace astralog {
+
+bool SystemInitializer::load_sensor_config(const std::string &yaml_path) {
+  // TODO: Implement YAML parsing for sensor configuration
+  return true;
+}
+
+bool SystemInitializer::load_rules_config(const std::string &json_path) {
+  // TODO: Implement JSON parsing for rules configuration
+  return true;
+}
+
+} // namespace astralog
